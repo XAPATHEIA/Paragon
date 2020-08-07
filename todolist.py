511c171631f0f82bfee917fa973e67acb01c2c2c
XAPATHEIA <54541033+XAPATHEIA@users.noreply.github.com>
2020-08-07 16:41:00 +0100

    Update todolist.py

--- todolist.py
+++ todolist.py
@@ -1,3 +1,19 @@
 import json
+import time
 from TODOListCLI import taskencode
-from TODOListCLI import task
+from TODOListCLI.task import Task
+
+total_tasks = []
+time.sleep(1)
+print("""
+******** To-Do List ********
+
+
+____________________________
+""")
+
+# Initiating loop to allow for consecutive additions/removals of tasks
+while True:
+    if not total_tasks:
+
+        
