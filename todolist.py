2e34cf25253fd5ee588b8798883af8a135ea7645
XAPATHEIA <54541033+XAPATHEIA@users.noreply.github.com>
2020-08-20 13:45:34 +0100

    constructing interface

--- todolist.py
+++ todolist.py
@@ -1,19 +1,55 @@
 import json
 import time
+import datetime
 from TODOListCLI import taskencode
-from TODOListCLI.task import Task
 
-total_tasks = []
-time.sleep(1)
-print("""
-******** To-Do List ********
+tasks = {}
+today = datetime.date.today()
+cd = today.strftime("%d/%m/%Y")
+
+description = {"task": "Finish Homework",
+               "completed": False}
+instantiation = {1: description}
+tasks[cd] = instantiation
+print(tasks)
 
 
-____________________________
-""")
+def interface():
+    if tasks[cd].keys():
+        u_query = int(input(("""What would you like to do?:
+1. Add Task
+2. Mark Completion
+3. Remove Task
+4. Remove All
+0. Exit
+""")))
+    else:
+        u_query = int(input(("""What would you like to do?:
+1. Add Task
+0. Exit
+""")))
+    return u_query
+
+
+def add_task():
+    if cd not in tasks.keys():
+
+
+
 
 # Initiating loop to allow for consecutive additions/removals of tasks
 while True:
-    if not total_tasks:
-
-        
+    print("******** To-Do List ********")
+    print("____________________________\n")
+    time.sleep(1)
+    query = interface()
+    if query == 1:
+        pass
+    if query == 2:
+        pass
+    if query == 3:
+        pass
+    if query == 4:
+        pass
+    if query == 0:
+        break
